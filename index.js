d2340582acf27e999e6b2ee8c753095566064b38
ZachyDev <mosetizachary001@gmail.com>
2020-10-19 13:18:28 +0300

    firebase auth

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
     messagingSenderId: "163799128909",
     appId: "1:163799128909:web:5b5ec1fb07c96b82f83504"
 };
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 
@@ -38,6 +38,24 @@ const getData = (docId) => {
                 // update the DOM
                 dataDiv.innerHTML = users;  
             });
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+
+// authetication
+
+const createUserWithEmailAndPassword = () => {
+    // get the input fields
+    const email = document.getElementById('email').value;
+
+    const pass = document.getElementById('pass').value;
+
+    firebase.auth().createUserWithEmailAndPassword(email,pass)
+        .then(res => {
+            console.log(res);
         })
         .catch(err => {
             console.log(err);
